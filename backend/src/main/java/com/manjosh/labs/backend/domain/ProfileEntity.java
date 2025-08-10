2ed7fc85f3b45e276289f66e66a700fe26353bf5
manjosh1990 <manjosh.1990@yahoo.com>
2025-08-10 20:36:24 +0530

    changed spotless formatter

--- backend/src/main/java/com/manjosh/labs/backend/domain/ProfileEntity.java
+++ backend/src/main/java/com/manjosh/labs/backend/domain/ProfileEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,8 +16,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tbl_profiles")
@@ -28,23 +27,29 @@ import java.time.LocalDateTime;
 @Builder
 public class ProfileEntity {
     @Id
-    @SequenceGenerator(name = "ft_id_seq_gen",sequenceName = "ft_seq")
+    @SequenceGenerator(name = "ft_id_seq_gen", sequenceName = "ft_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ft_id_seq_gen")
     @Column(name = "profile_id")
     private Long id;
+
     @Column(nullable = false)
     private String fullName;
+
     @Column(unique = true, nullable = false)
     private String email;
+
     @Column(nullable = false)
     private String password;
+
     private String profileImageUrl;
 
     @Column(updatable = false)
     @CreationTimestamp
     private LocalDateTime createdAt;
+
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
     private boolean isActive;
     private String activationToken;
 }
