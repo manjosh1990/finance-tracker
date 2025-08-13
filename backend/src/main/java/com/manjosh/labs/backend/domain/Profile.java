c0269dd701b2e44dacf22761299ca7c56209ebac
manjosh1990 <manjosh.1990@yahoo.com>
2025-08-13 21:31:44 +0530

    Profile DTO

---
+++ backend/src/main/java/com/manjosh/labs/backend/domain/Profile.java
@@ -0,0 +1,11 @@
+package com.manjosh.labs.backend.domain;
+
+public record Profile(
+        Long id,
+        String fullName,
+        String email,
+        String password,
+        String profileImageUrl
+        , long createdAt, long updatedAt
+) {
+}
