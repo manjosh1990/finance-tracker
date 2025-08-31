233a1b1aa21508d4029b0021f002157763bfa6e0
manjosh1990 <manjosh.1990@yahoo.com>
2025-08-31 12:31:04 +0530

    build fix

--- backend/src/main/java/com/manjosh/labs/backend/domain/NotificationService.java
+++ backend/src/main/java/com/manjosh/labs/backend/domain/NotificationService.java
@@ -4,15 +4,14 @@ import com.manjosh.labs.backend.domain.expense.Expense;
 import com.manjosh.labs.backend.domain.expense.ExpenseService;
 import com.manjosh.labs.backend.domain.profile.Profile;
 import com.manjosh.labs.backend.domain.profile.ProfileService;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
--- backend/src/main/java/com/manjosh/labs/backend/web/exception/ServiceException.java
+++ backend/src/main/java/com/manjosh/labs/backend/web/exception/ServiceException.java
@@ -4,4 +4,12 @@ public class ServiceException extends RuntimeException {
     public ServiceException(String message) {
         super(message);
     }
+
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
