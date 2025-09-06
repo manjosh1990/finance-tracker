f0a9a0d6becad56477038dfdcf61ba2220580cf6
manjosh1990 <manjosh.1990@yahoo.com>
2025-09-06 15:10:56 +0530

    login api implementation

--- xpensive-gui/src/pages/Login.jsx
+++ xpensive-gui/src/pages/Login.jsx
@@ -1,14 +1,61 @@
-import {useState} from "react";
+import {useContext, useState} from "react";
 import Input from "../components/Input.jsx";
+import {validateEmail} from "../util/validation.js";
+import axiosConfig from "../util/axiosConfig.js";
+import {API_ENDPOINTS} from "../util/apiEndpoints.js";
+import {AppContext} from "../context/AppContext.jsx";
+import {useNavigate} from "react-router-dom";
+import {LoaderCircle} from "lucide-react";
+
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
-
+    const [loading, setLoading] = useState(false);
+    const {setUser} = useContext(AppContext);
+    const navigate = useNavigate();
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
+        //validation
+        if (!validateEmail(email)) {
+            setError("Please enter a valid email address.");
+            setLoading(false);
+            return
+        }
+        if (!password.trim()) {
+            setError("Please enter your password.");
+            setLoading(false);
+            return
+        }
+        setError("");
+        //login api call
 
+        try {
+            const res = await axiosConfig.post(API_ENDPOINTS.LOGIN, {
+                email,
+                password
+            });
+            console.log(res);
+            if (res.status === 200) {
+                const {token, user} = res.data;
+                if (token) {
+                    localStorage.setItem("token", token);
+                    setUser(user);
+                    navigate("/dashboard");
+                }
+            }
+        } catch (err) {
+            console.log("error in login ", err);
+            if (err.response && err.response.data.message) {
+                setError(err.response.data.message);
+            } else {
+                setError("Something went wrong");
+            }
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
@@ -20,11 +67,11 @@ const Login = () => {
                     className="rounded-xl sm:rounded-2xl p-[1.5px] sm:p-[2px] bg-brand-gradient shadow-xl sm:shadow-2xl sm:max-h-[90vh] sm:overflow-y-auto">
                     <div className="rounded-xl sm:rounded-2xl p-4 sm:p-8 bg-bg">
                         <h3 className="text-xl sm:text-2xl font-semibold text-center mb-2 text-gradient">
-  Sign in to your account
-</h3>
-<p className="text-sm sm:text-base text-center mb-6 sm:mb-8 text-gradient">
-  Continue where you left off and stay in control of your finances.
-</p>
+                            Sign in to your account
+                        </h3>
+                        <p className="text-sm sm:text-base text-center mb-6 sm:mb-8 text-gradient">
+                            Continue where you left off and stay in control of your finances.
+                        </p>
                         <form className="space-y-4" onSubmit={handleSubmit}>
                             <div className="flex justify-center mb-4 sm:mb-6">
                                 {/*profile picture*/}
@@ -57,8 +104,17 @@ const Login = () => {
                                     <p className="text-red-800 text-sm text-center bg-red-50 p-2 mb-2 rounded">{error}</p>
                                 )}
                                 <button type="submit"
-                                        className="btn-primary w-full py-3 sm:py-3.5 text-base sm:text-lg font-medium">
-                                    LOGIN
+                                        className={`btn-primary w-full py-3 sm:py-3.5 text-base sm:text-lg font-medium flex items-center justify-center gap-2 ${loading ? 'opacity-60 cursor-not-allowed' : ''}`}
+                                        disabled={loading}>
+                                    {loading ? (
+                                        <>
+                                            <LoaderCircle className="animate-spin w-5 h-5"/>
+                                            Logging in...
+                                        </>
+                                    ) : (
+                                        <>
+                                            SIGN IN</>
+                                    )}
                                 </button>
                                 <p className="text-sm sm:text-base text-gradient text-center mt-4 sm:mt-6">
                                     Don't have an account? <a href="/signup"
